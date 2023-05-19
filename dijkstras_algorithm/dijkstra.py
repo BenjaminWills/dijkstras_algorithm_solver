a4ae283bd346a9cca2254a5ff1b9fbaa6fa75eae
Benjaminwills <benjaminwills047@gmail.com>
2023-05-19 20:36:13 +0100

    first commit

---
+++ dijkstras_algorithm/dijkstra.py
@@ -0,0 +1,77 @@
+import numpy as np
+import copy
+
+from typing import List
+
+
+class Network:
+    def __init__(
+        self, adjacency_matrix: np.array, node_names: List[str] = None
+    ) -> None:
+        self.adjacency_matrix = adjacency_matrix
+        self.num_nodes = adjacency_matrix.shape[0]
+        self.node_names = node_names or range(self.num_nodes)
+
+    def get_shortest_distances(self, node: int) -> np.array:
+        """Uses Dijkstras algorithm to find the shortest distances to all of the nodes
+
+        Parameters
+        ----------
+        node : int
+            Starting node
+
+        Returns
+        -------
+        np.array
+            An array whose elements correspond to the nodes
+        """
+        unvisited_nodes = {n: 1 for n in range(0, self.num_nodes)}
+        visited_nodes = {n: 0 for n in range(0, self.num_nodes)}
+
+        # Visit source node
+        unvisited_nodes[node] = 0
+        visited_nodes[node] = 1
+
+        last_visited = node
+
+        # Get initial distance matrix
+        distance_matrix = self.init_distance_matrix(node)
+        # Start loop
+        while list(unvisited_nodes.values()) != [0] * self.num_nodes:
+            # Find nearest neighbours to the most recent visited node
+            nearest_neighbours = copy.deepcopy(
+                self.adjacency_matrix[last_visited, :]
+            )  # Deep copy to avoid scoping issues
+
+            maximus = nearest_neighbours.max()
+            nearest_neighbours[nearest_neighbours == 0] = (
+                maximus + 1
+            )  # Avoiding issue of 0 values
+            nearest_neighbour, nearest_neighbour_distance = (
+                nearest_neighbours.argmin(),  # Nearest neighbour node name
+                nearest_neighbours.min(),  # Nearest neighbout distance
+            )
+
+            new_distance = distance_matrix[last_visited] + nearest_neighbour_distance
+
+            if distance_matrix[nearest_neighbour] > new_distance:
+                distance_matrix[nearest_neighbour] = new_distance
+
+            unvisited_nodes[nearest_neighbour] = 0
+            visited_nodes[nearest_neighbour] = 1
+            last_visited = nearest_neighbour
+        return distance_matrix
+
+    def init_distance_matrix(self, source_node: int) -> np.array:
+        # Initialise distance matrix
+        distance_matrix = np.full(self.adjacency_matrix.shape[0], np.inf)
+        distance_matrix[source_node] = 0
+        return distance_matrix
+
+
+if __name__ == "__main__":
+    adjacency_matrix = np.array(
+        [[0, 3, 0, 1], [0, 0, 1, 0], [0, 0, 0, 0], [0, 1, 2, 0]]
+    )
+    network = Network(adjacency_matrix)
+    print(network.get_shortest_distances(0))
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Dijkstra`s algorithm visualiser
+
+This is just a fun tool to visualise how Dijkstra`s algorithm works.
+
+## The algorithm
+
+Dijkstra's algorithm allows us to find the shortest distance between any two nodes on a graph.
